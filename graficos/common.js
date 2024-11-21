a48ce1a51d9ee4b1b4d668aeff7363ca274f6e73
Robert Simão dos Santos <163461719+Dillax@users.noreply.github.com>
2024-11-21 14:54:28 -0300

    d

--- graficos/common.js
+++ graficos/common.js
@@ -3,3 +3,9 @@ const getCSS = (variavel) => {
 }
  
 export {getCSS}
+const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+}
+export {getCSS tickConfig}
